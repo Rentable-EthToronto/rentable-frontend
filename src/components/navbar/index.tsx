a1f38e054d9d011b8c8c9dc90ed34136d927c5bd
brucewang <brucewangyq@gmail.com>
2022-08-09 12:07:16 -0400

    feat: add navbar

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+export const Navbar = () => {
+  return (
+    <div className="flex justify-center items-center">
+      Logo <ConnectButton />
+    </div>
+  );
+};
+export default Navbar;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,18 @@
 import type { NextPage } from "next";
 import type { FC } from "react";
-import { ConnectButton } from "@rainbow-me/rainbowkit";
-import Header from "../components/Header";
+
 import { useWeb3 } from "../context/web3Context";
+import Layout from "../components/layout";
 
 const Home: NextPage = () => {
   const { address } = useWeb3();
-  console.log(address);
-  return (
-    <div className="py-6 justify-center text-center">
-      <Header />
-      <div className="flex justify-center">
-        <ConnectButton />
-      </div>
 
-      <h1 className="text-4xl font-bold mt-6">🚀 address: {address}</h1>
-    </div>
+  return (
+    <Layout>
+      <div className="py-6 justify-center text-center">
+        <h1 className="text-4xl font-bold mt-6">🚀 address: {address}</h1>
+      </div>
+    </Layout>
   );
 };
 
