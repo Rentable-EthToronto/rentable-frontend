078b3ed478ba377bab6b59aced5b491d5158cfc8
brucewang <brucewangyq@gmail.com>
2022-08-10 00:57:53 -0400

    feat: change logo

--- src/components/navbar/logo.tsx
+++ src/components/navbar/logo.tsx
@@ -6,7 +6,16 @@ const Logo = () => {
   return (
     <Link href="/" id="home">
       <div className="w-24">
-        <Image src={logo} alt="Rentable Logo" />
+        {/* <Image src={logo} alt="Rentable Logo" /> */}
+        <a
+          className="flex items-center text-green-400 no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
+          href="#"
+        >
+          Rent
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500">
+            able
+          </span>
+        </a>
       </div>
     </Link>
   );
