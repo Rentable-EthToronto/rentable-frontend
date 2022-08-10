d9fa4033bf3101f5d5c0f7aa2ad938772e889e12
brucewang <brucewangyq@gmail.com>
2022-08-10 00:51:05 -0400

    feat: add empty NFT page

--- src/components/NftCard.tsx
+++ src/components/NftCard.tsx
@@ -7,15 +7,27 @@ export const NftCard: React.FC<{
   return (
     <div className="m-9 flex md:w-1/4 w-1/2 flex-col rounded-3xl border border-spacing-2 border-slate-200 text-white hover:shadow-lg bg-slate-800">
       <div className="rounded-md">
-        <img
-          src={image}
-          style={{
-            width: "100%",
-            height: "100%",
-            borderTopRightRadius: 24,
-            borderTopLeftRadius: 24,
-          }}
-        />
+        {image ? (
+          <img
+            src={image}
+            style={{
+              width: "100%",
+              height: "100%",
+              borderTopRightRadius: 24,
+              borderTopLeftRadius: 24,
+            }}
+          />
+        ) : (
+          <img
+            src={"/emptyNFT.jpeg"}
+            style={{
+              width: "100%",
+              height: "100%",
+              borderTopRightRadius: 24,
+              borderTopLeftRadius: 24,
+            }}
+          />
+        )}
       </div>
       <div className="p-6">{children}</div>
     </div>
