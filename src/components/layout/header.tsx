ecfdf3080044e602997145551ee00958080533c3
brucewang <brucewangyq@gmail.com>
2022-08-10 00:42:59 -0400

    feat: change navbar styles

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -2,7 +2,7 @@ import Navbar from "../navbar";
 
 const Header = () => {
   return (
-    <header className="sticky top-0 z-20">
+    <header className="sticky top-0 z-20 bg-[#18181F] opacity-90 pt-1 pb-2">
       <div className="max-w-5xl mx-auto">
         <Navbar />
       </div>
