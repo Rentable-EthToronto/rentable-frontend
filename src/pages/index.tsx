54abe8b337f0c65c073563c6677e408e840997b1
brucewang <brucewangyq@gmail.com>
2022-08-09 22:13:34 -0400

    feat: change bored ape

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,7 +25,7 @@ const Home: NextPage = () => {
         <div className="w-full xl:w-3/5 p-12 overflow-hidden">
           <img
             className="mx-auto w-full md:w-4/5 transform -rotate-6 transition hover:scale-105 duration-700 ease-in-out hover:rotate-6"
-            src="images.jpeg"
+            src="bored-ape.png"
           />
         </div>
       </div>
